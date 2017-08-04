4e2e20ac0f777ba56c796662b525c2ad21928ed1
Greg Trevellick <gtrevellick@googlemail.com>
2017-08-04 18:38:49 +0100

    .

--- SolutionOpenPopUp.Tests/Class1.cs
+++ SolutionOpenPopUp.Tests/Class1.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace SolutionOpenPopUp.Tests
 {
+    [TestFixture]
     public class Class1
     {
+        [TestCase()]
+        public void Test1()
+        {
+
+        }
     }
 }
