120b7b88eace76a6fc7d25b893baa9a291b2db53
Greg Trevellick <gtrevellick@googlemail.com>
2017-08-11 21:37:44 +0100

    .

--- SolutionOpenPopUp/source.extension.cs
+++ SolutionOpenPopUp/source.extension.cs
@@ -9,10 +9,10 @@ namespace SolutionOpenPopUp
     {
         public const string Id = "9ea776a6-e317-4ba8-a957-fa59958ec214";
         public const string Name = "Solution Open Pop Up";
-        public const string Description = @"Display the contents of certain text files in a solution folder in a pop-up when the solution is opened. A handy way to share solution-specific gotchas, non-critical team messages or your own personal notes for a solution. 
+        public const string Description = @"Display the contents of certain text files when the solution is opened in a pop-up window. A handy way to share solution-specific gotchas, non-critical team messages or your own personal notes for a solution. 
 ";
         public const string Language = "en-US";
-        public const string Version = "1.0.20";
+        public const string Version = "1.0.21";
         public const string Author = "Greg Trevellick";
         public const string Tags = "solution open, popup, pop-up, pop up, gotcha, gotchas";
     }
